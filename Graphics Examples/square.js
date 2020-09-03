e3b2a52f591ac27bd3f502e3de08976d0b00bda7
micodian <micodian83@gmail.com>
2020-09-02 21:20:32 -0500

    changed all files to the correct codes

--- Graphics Examples/square.js
+++ Graphics Examples/square.js
@@ -1,49 +1,48 @@
+var canvas;
 var gl;
-var points;
-
-
-
 
 window.onload = function init() {
-    var canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById( "gl-canvas" );
 
-    gl= WebGLUtils.setupWebGL( canvas );
-    if(!gl){
-        alert("WebGL isnt available");
-    }
-    
- //four Vertices
-var vertices = [
-    vec2(-0.5,-0.5),
-    vec2(-0.5,0.5),
-    vec2(0.5,0.5),
-    vec2(0.5,-0.5)
-];
-//configure WebGL
+    gl = gl = canvas.getContext('webgl2');
+    if ( !gl ) { alert( "WebGL 2.0 isn't available" ); }
 
-gl.viewport(0,0,canvas.width,canvas.height);
-gl.clearColor(0.0,0.0,0.0,1.0);
-//load shaders and initialize attribute buffers
-var program = initShaders(gl,"vertex-shader","fragment-shader");
-gl.useProgram(program);
+    // Four Vertices
 
-//load the data into the GPU
-var bufferId = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices),gl.STATIC_DRAW);
-
-//associate out shader variables with out data buffer
-
-var vPosition = gl.getAttribLocation(program, "vPosition");
-gl.vertexAttribPointer(vPosition,2,gl.FLOAT,false,0,0);
-gl.enableVertexAttribArray(vPosition);
+    var vertices = [
+        vec2( -0.5, -0.5 ),
+        vec2(  -0.5,  0.5 ),
+        vec2(  0.5, 0.5 ),
+        vec2( 0.5, -0.5)
+    ];
 
 
-render();
+     //  Configure WebGL
 
+ gl.viewport( 0, 0, canvas.width, canvas.height );
+ gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+  //  Load shaders and initialize attribute buffers
+
+ var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+ gl.useProgram( program );
+
+  // Load the data into the GPU
+
+ var bufferId = gl.createBuffer();
+ gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+ gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+  // Associate out shader variable with our data buffer
+
+ var aPosition = gl.getAttribLocation( program, "aPosition" );
+ gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
+ gl.enableVertexAttribArray(aPosition);
+
+     render();
 };
 
-function render(){
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLE_FAN,0,4);
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
 }
